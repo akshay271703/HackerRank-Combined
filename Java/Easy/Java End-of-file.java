b8b0ad7abda9c8f4bfa717d8682f77663b36d815
Akshay Bhargava <toakshay.official@gmail.com>
2020-03-30 10:44:52 +0530

    Score 10.0
    Language : Java-7

---
+++ Java/Easy/Java End-of-file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner Sc = new Scanner(System.in);
+        int i=0;
+        while(Sc.hasNext())
+        {
+            i++;
+            System.out.println(i+" "+Sc.nextLine());
+        }
+    }
+}
